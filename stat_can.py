af02ee098e7e66acbd34ad53d9fefd419332a76f
Brian <94982128+laoyuanx@users.noreply.github.com>
2023-06-21 16:42:38 -0700

    Update stat_can.py

--- stat_can.py
+++ stat_can.py
@@ -6,13 +6,6 @@ sc = StatsCan()
 #load consumer price index table to df
 df = sc.table_to_df("18-10-0004-01")
 st.title('Consumner Price Index - Canada')
-#st.dataframe(df)
-#st.text('Statistics Canada. Table 18-10-0004-01  Consumer Price Index, monthly, not seasonally adjusted')
-
-def get_fruityvice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-  fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-  return fruityvice_normalized
 
 # User provide keyworkds as filter
 products_selected = st.multiselect("Pick some keywords:",set(df['Products and product groups']),['All-items','Food','Meat'])
@@ -33,9 +26,9 @@ st.line_chart(chart_data)
 
 # Display the table
 st.dataframe(chart_data)
+
 st.stop()
 
-streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('Add a keyword')
   if not fruit_choice:
