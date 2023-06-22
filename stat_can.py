8ffc47a04471007ea5e3ed3b23686f268e230023
Brian <94982128+laoyuanx@users.noreply.github.com>
2023-06-22 14:40:14 -0700

    Update stat_can.py

--- stat_can.py
+++ stat_can.py
@@ -11,45 +11,47 @@ sc = StatsCan()
 START = "2018-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
-st.title('Consumer Price Index - Canada')
+st.title('Consumer Price Index Forecast - Canada')
 
 #stocks = ('GOOG', 'AAPL', 'MSFT', 'GME')
 #selected_stock = st.selectbox('Select dataset for prediction', stocks)
 #load consumer price index table to df
+@st.cache
 df = sc.table_to_df("18-10-0004-01")
 
 # User selects series for prediction
 products_selected = st.selectbox("Select series for prediction:",set(df['Products and product groups']))
 
-st.stop()
-n_years = st.slider('Years of prediction:', 1, 4)
-period = n_years * 365
+n_years = st.slider('Years of prediction:', 1, 5)
+period = n_years * 12
+#data_load_state = st.text('Loading data...')
+#data = load_data(selected_stock)
+#st.subheader('Raw data')
+#st.write(data.tail())
 
+# Transform original df to chart_data
+data=(df
+ #.pivot(index=['GEO','Products and product groups'], columns='REF_DATE', values='VALUE')
+ .rename(columns = {"Products and product groups":"ITEM"})
+ .query("ITEM in @products_selected and GEO == 'Canada' and REF_DATE >= START")
+ #.query("ITEM in @products_selected and GEO == 'Canada' and REF_DATE >= '2022-01-01'")
+ .pivot(index=['REF_DATE'], columns='ITEM', values='VALUE')
+ .reset_index()
+ #.set_index('REF_DATE')
+ #.plot()
+)
 
-@st.cache
-def load_data(ticker):
-    data = yf.download(ticker, START, TODAY)
-    data.reset_index(inplace=True)
-    return data
-
-	
-data_load_state = st.text('Loading data...')
-data = load_data(selected_stock)
-data_load_state.text('Loading data... done!')
-
-st.subheader('Raw data')
-st.write(data.tail())
-
+#data_load_state.text('Loading data... done!')
 # Plot raw data
-def plot_raw_data():
+def plot_raw_data(products_selected):
 	fig = go.Figure()
-	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
-	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
+	fig.add_trace(go.Scatter(x=data['REF_DATE'], y=data['VALUE'], name=products_selected))
+	#fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
 	fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
 	st.plotly_chart(fig)
 	
 plot_raw_data()
-
+st.stop()
 # Predict forecast with Prophet.
 df_train = data[['Date','Close']]
 df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
@@ -72,16 +74,7 @@ fig2 = m.plot_components(forecast)
 st.write(fig2)
 
 
-# Transform original df to chart_data
-chart_data=(df
- #.pivot(index=['GEO','Products and product groups'], columns='REF_DATE', values='VALUE')
- .rename(columns = {"Products and product groups":"ITEM"})
- .query("ITEM in @products_selected and GEO == 'Canada' and REF_DATE >= '2022-01-01'")
- .pivot(index=['REF_DATE'], columns='ITEM', values='VALUE')
- .reset_index()
- .set_index('REF_DATE')
- #.plot()
-)
+
 
 #Dispaly a line chart
 st.line_chart(chart_data)
